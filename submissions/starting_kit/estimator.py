0a5f9f0437e0944c77cdfd14f3e16d4967c33c8f
Eya JLASSI_Codes <eya.jlassi@ensta-paris.fr>
2025-03-09 21:31:08 +0100

    Delete submissions/starting_kit/estimator.py

--- submissions/starting_kit/estimator.py
+++
@@ -1,9 +0,0 @@
-from sklearn.dummy import DummyClassifier
-from sklearn.pipeline import make_pipeline
-from sklearn.impute import SimpleImputer
-
-def get_estimator():
-    return make_pipeline(
-        SimpleImputer(strategy='most_frequent'),  
-        DummyClassifier(strategy='most_frequent', random_state=60)  
-    )
